19eaf26d9e6a459b7408c73f17476b4fd4ffe312
Neosoulink <neodarksoulink@gmail.com>
2022-08-31 11:21:28 +0200

    refactor: group cubes

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import "./assets/css/style.css";
 const APP = initThreeJs({ enableOrbit: true });
 const CUBE_CLONE = Cube.clone();
 const AXES_HELPER = new THREE.AxesHelper(5);
+const CUBES_GROUP = new THREE.Group();
 
 // SCALES
 Cube.scale.set(0.5, 0.5, 0.5);
@@ -23,9 +24,13 @@ CUBE_CLONE.position.set(-3, -1, 0);
 Cube.rotation.reorder("YXZ");
 Cube.rotation.set(Math.PI / 5, 3, 3);
 CUBE_CLONE.rotation.set(Math.PI / 2, -1, 0, "YXZ");
+CUBES_GROUP.rotation.z = 3;
 
-APP.scene.add(Cube);
-APP.scene.add(CUBE_CLONE);
+//
+CUBES_GROUP.add(Cube, CUBE_CLONE);
 APP.scene.add(AXES_HELPER);
+APP.scene.add(CUBES_GROUP);
 
-APP.camera.position.z = 3;
+//
+APP.camera.position.z = 10;
+APP.camera.lookAt(CUBES_GROUP.position);
