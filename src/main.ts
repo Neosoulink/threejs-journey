e375990d4e4105401927e514c9f41d531552fe23
Neosoulink <neodarksoulink@gmail.com>
2022-08-31 10:58:58 +0200

    refator: update cube ratations

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,17 @@ const APP = initThreeJs({ enableOrbit: true });
 const CUBE_CLONE = Cube.clone();
 const AXES_HELPER = new THREE.AxesHelper(5);
 
+// SCALES
 Cube.scale.set(0.5, 0.5, 0.5);
+
+// POSITIONS
 Cube.position.set(2, 0, 0);
 CUBE_CLONE.position.set(-3, -1, 0);
+
+// ROTATIONS
+Cube.rotation.reorder("YXZ");
+Cube.rotation.set(Math.PI / 5, 3, 3);
+CUBE_CLONE.rotation.set(Math.PI / 2, -1, 0, "YXZ");
 
 APP.scene.add(Cube);
 APP.scene.add(CUBE_CLONE);
