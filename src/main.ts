c112f0f8931a531d85d43289ef6f09f2d8e7b795
Neosoulink <neodarksoulink@gmail.com>
2022-08-31 10:41:31 +0200

    refator: update cube perspective

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import * as THREE from "three";
+
 // HELPERS
 import initThreeJs from "./helpers/initThreeJs";
-
 // COMPONENTS
 import Cube from "./components/Cube";
 
@@ -9,9 +10,14 @@ import "./assets/css/style.css";
 
 const APP = initThreeJs({ enableOrbit: true });
 const CUBE_CLONE = Cube.clone();
-CUBE_CLONE.position.x = -3;
+const AXES_HELPER = new THREE.AxesHelper(5);
+
+Cube.scale.set(0.5, 0.5, 0.5);
+Cube.position.set(2, 0, 0);
+CUBE_CLONE.position.set(-3, -1, 0);
 
 APP.scene.add(Cube);
 APP.scene.add(CUBE_CLONE);
+APP.scene.add(AXES_HELPER);
 
 APP.camera.position.z = 3;
