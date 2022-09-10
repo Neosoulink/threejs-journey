ac4e739dd1405714d17bc06f30445987fd3d71ff
Neosoulink <neodarksoulink@gmail.com>
2022-09-10 13:22:03 +0200

    feat: camera animation on mouse mouve

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ const CUBES_GROUP = new THREE.Group();
 Cube.scale.set(0.5, 0.5, 0.5);
 
 // POSITIONS
-Cube.position.set(2, 0, 0);
+Cube.position.set(0, 0, 0);
 CUBE_CLONE.position.set(-3, -1, 0);
 
 // ROTATIONS
@@ -47,6 +47,16 @@ APP.camera.position.z = 10;
 // GSAP.to(CUBES_GROUP.position, { duration: 0.2, delay: 1, x: 1 });
 // GSAP.to(CUBES_GROUP.position, { duration: 0.2, delay: 2, x: 0 });
 
+// CURSOR ANIMATION
+const CURSOR_POS = {
+	x: 0,
+	y: 0,
+};
+window.addEventListener("mousemove", (e) => {
+	CURSOR_POS.x = e.clientX / APP.sceneSizes.width - 0.5;
+	CURSOR_POS.y = e.clientY / APP.sceneSizes.height - 0.5;
+});
+
 APP.animate(() => {
 	// Animation using native js date
 	// const CURRENT_TIME = Date.now();
@@ -57,5 +67,10 @@ APP.animate(() => {
 	const ELAPSED_TIME = ANIMATION_CLOCK.getElapsedTime();
 	CUBES_GROUP.rotation.y = Math.sin(ELAPSED_TIME);
 	CUBES_GROUP.rotation.x = Math.cos(ELAPSED_TIME);
-	// APP.camera.lookAt(CUBE_CLONE.position);
+
+	// CAMERA ANIMATION
+	APP.camera.position.x = Math.sin(CURSOR_POS.x * Math.PI * 2) * 5;
+	APP.camera.position.z = Math.cos(CURSOR_POS.x * Math.PI * 2) * 5;
+	APP.camera.position.y = CURSOR_POS.y * 10;
+	APP.camera.lookAt(new THREE.Vector3());
 });
