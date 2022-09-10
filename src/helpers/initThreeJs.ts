00885304cb068475d7279ba2650e968a7e534c49
Neosoulink <neodarksoulink@gmail.com>
2022-09-10 13:22:50 +0200

    refactor: enable control prop on three initer

--- src/helpers/initThreeJs.ts
+++ src/helpers/initThreeJs.ts
@@ -5,12 +5,18 @@ export interface initThreeProps {
 	enableAnimation?: boolean;
 	enableOrbit?: boolean;
 	axesSizes?: number;
+	sceneSizes?: {
+		width: number;
+		height: number;
+	};
+	control?: OrbitControls;
 }
 
 // DEFS
 let scene: THREE.Scene;
 let camera: THREE.Camera;
 let renderer: THREE.WebGL1Renderer;
+let control: OrbitControls;
 
 // FUNCTIONS
 export function animate(callback: () => any = () => {}) {
@@ -20,7 +26,11 @@ export function animate(callback: () => any = () => {}) {
 }
 
 export default (props?: initThreeProps) => {
-	const app = document.querySelector<HTMLDivElement>("#app")!;
+	const APP = document.querySelector<HTMLDivElement>("#app")!;
+	const SCENE_SIZES: initThreeProps["sceneSizes"] = props?.sceneSizes ?? {
+		width: window.innerWidth,
+		height: window.innerHeight,
+	};
 
 	// SCENE & CAMERA
 	scene = new THREE.Scene();
@@ -28,13 +38,13 @@ export default (props?: initThreeProps) => {
 	// Perspective camera
 	camera = new THREE.PerspectiveCamera(
 		75,
-		window.innerWidth / window.innerHeight,
+		SCENE_SIZES.width / SCENE_SIZES.height,
 		0.1,
 		1000
 	);
 
 	// Orthographic Camera
-	// const ASPECT_RATIO = window.innerWidth / window.innerHeight;
+	// const ASPECT_RATIO = SCENE_SIZES.width / SCENE_SIZES.height;
 	// camera = new THREE.OrthographicCamera(
 	// 	-1 * ASPECT_RATIO,
 	// 	1 * ASPECT_RATIO,
@@ -46,14 +56,13 @@ export default (props?: initThreeProps) => {
 	// console.log(ASPECT_RATIO);
 
 	renderer = new THREE.WebGL1Renderer({ antialias: true });
-	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(SCENE_SIZES.width, SCENE_SIZES.height);
 
-	app.appendChild(renderer.domElement);
+	APP.appendChild(renderer.domElement);
 
 	// ORBIT CONTROL
 	if (props?.enableOrbit) {
-		// @ts-ignore: Will be used later
-		const orbitControls = new OrbitControls(camera, renderer.domElement);
+		control = new OrbitControls(camera, renderer.domElement);
 	}
 
 	if (typeof props?.axesSizes === "number") {
@@ -66,5 +75,7 @@ export default (props?: initThreeProps) => {
 		camera,
 		renderer,
 		animate,
+		sceneSizes: SCENE_SIZES,
+		control,
 	};
 };
