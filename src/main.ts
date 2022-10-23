f1a716e5b4d85ab7c6565da9ead708f7fcc7e667
Neosoulink <neodarksoulink@gmail.com>
2022-10-23 13:07:13 +0200

    style: clean code

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,32 @@ import * as THREE from "three";
 import GUI from "lil-gui";
 import GSAP from "gsap";
 
-// HELPERS
+/* HELPERS */
 import initThreeJs from "./helpers/initThreeJs";
 
-// COMPONENTS
+/* COMPONENTS */
 import Cube from "./components/Cube";
 
-// STYLES
+/* STYLES */
 import "./assets/css/style.css";
 
-// IMAGES
+/* IMAGES */
+/* Door images */
 // import doorAlphaImg from "./assets/img/textures/door/alpha.jpg";
 // import doorAmbientOcclusionImg from "./assets/img/textures/door/ambientOcclusion.jpg";
-// import minecraftImg from "./assets/img/textures/minecraft.png";
 import doorDoorImg from "./assets/img/textures/door/color.jpg";
 // import doorHeightImg from "./assets/img/textures/door/height.jpg";
 // import doorMetalnessImg from "./assets/img/textures/door/metalness.jpg";
 // import doorNormalImg from "./assets/img/textures/door/normal.jpg";
 // import doorRoughnessImg from "./assets/img/textures/door/roughness.jpg";
 
+/* Matcaps images */
 // import matcaps1Img from "./assets/img/textures/matcaps/3.png";
+
+/* Gradients images */
 // import gradientsImg from "./assets/img/textures/gradients/5.jpg";
 
+/* Environment map images */
 import nxEnvImg from "./assets/img/textures/environmentMaps/0/nx.jpg";
 import nyEnvImg from "./assets/img/textures/environmentMaps/0/ny.jpg";
 import nzEnvImg from "./assets/img/textures/environmentMaps/0/nz.jpg";
@@ -31,26 +35,18 @@ import pxEnvImg from "./assets/img/textures/environmentMaps/0/px.jpg";
 import pyEnvImg from "./assets/img/textures/environmentMaps/0/py.jpg";
 import pzEnvImg from "./assets/img/textures/environmentMaps/0/pz.jpg";
 
-// DEBUGGER
-const _GUI = new GUI();
-
-// DATA
+/* DATA */
 const TRIANGLE_VERTICES_COUNT = 500;
 const TRIANGLE_VERTICES = new Float32Array(TRIANGLE_VERTICES_COUNT * 3 * 3);
-
+/* Fill vector 3 square line */
 for (let i = 0; i < TRIANGLE_VERTICES.length; i++) {
 	TRIANGLE_VERTICES[i] = (Math.random() - 0.5) * 4;
 }
 
-// TEXTURE
-// const CUBE_IMG = new Image();
-// const DOOR_COLOR_TEXTURE = new THREE.Texture(CUBE_IMG);
+/* DEBUGGERS */
+const _GUI = new GUI();
 
-// CUBE_IMG.onload = () => {
-// 	DOOR_COLOR_TEXTURE.needsUpdate = true;
-// };
-
-// CUBE_IMG.src = doorDoorImg;
+/* LOADING MANAGER */
 const LOADING_MANAGER = new THREE.LoadingManager();
 LOADING_MANAGER.onStart = () => {
 	console.log("on start loading");
@@ -65,6 +61,16 @@ LOADING_MANAGER.onError = () => {
 	console.log("Error triggered");
 };
 
+/* TEXTURES */
+/* Load texture with native javascript img class */
+// const CUBE_IMG = new Image();
+// const DOOR_COLOR_TEXTURE = new THREE.Texture(CUBE_IMG);
+// CUBE_IMG.onload = () => {
+// 	DOOR_COLOR_TEXTURE.needsUpdate = true;
+// };
+// CUBE_IMG.src = doorDoorImg;
+
+/* Load texture using THREE Textures loaders */
 const TEXTURE_LOADER = new THREE.TextureLoader(LOADING_MANAGER);
 const CUBE_TEXTURE_LOADER = new THREE.CubeTextureLoader(LOADING_MANAGER);
 // const DOOR_ALPHA_TEXTURE = TEXTURE_LOADER.load(doorAlphaImg);
@@ -94,6 +100,7 @@ const DOOR_COLOR_TEXTURE = TEXTURE_LOADER.load(doorDoorImg);
 // DOOR_COLOR_TEXTURE.center.y = 0.5;
 
 // DOOR_COLOR_TEXTURE.rotation = Math.PI / 4;
+
 DOOR_COLOR_TEXTURE.generateMipmaps = false;
 DOOR_COLOR_TEXTURE.minFilter = THREE.NearestFilter;
 DOOR_COLOR_TEXTURE.magFilter = THREE.NearestFilter;
@@ -107,21 +114,33 @@ const ENVIRNEMET_MAP_TEXTURE = CUBE_TEXTURE_LOADER.load([
 	nzEnvImg,
 ]);
 
-// FORMS
-const CubeClone = Cube.clone();
-CubeClone.material.color = new THREE.Color();
-CubeClone.material.map = DOOR_COLOR_TEXTURE;
+/* MATERIALS */
+/** Define Material using MeshBasicMaterial */
 // const NEW_MATER6IAL = new THREE.MeshBasicMaterial({map: SPHERE_1_TEXTURE});
+
+/** Define Material using MeshNormalMaterial */
 // const NEW_MATER6IAL = new THREE.MeshNormalMaterial();
+
+/** Define Material using MeshMatcapMaterial */
 // const NEW_MATER6IAL = new THREE.MeshMatcapMaterial({
 // 	matcap: SPHERE_1_TEXTURE,
 // });
+
+/** Define Material using MeshDepthMaterial */
 // const NEW_MATER6IAL = new THREE.MeshDepthMaterial();
+
+/** Define Material using MeshLambertMaterial */
 // const NEW_MATER6IAL = new THREE.MeshLambertMaterial();
+
+/** Define Material using MeshPhongMaterial */
 // const NEW_MATER6IAL = new THREE.MeshPhongMaterial();
+
+/** Define Material using MeshToonMaterial */
 // const NEW_MATER6IAL = new THREE.MeshToonMaterial({
 // 	gradientMap: GRADIENT_TEXTURE,
 // });
+
+/** Define Material using MeshStandardMaterial */
 const NEW_MATER6IAL = new THREE.MeshStandardMaterial({
 	metalness: 0.7,
 	roughness: 0.2,
@@ -147,30 +166,32 @@ const NEW_MATER6IAL = new THREE.MeshStandardMaterial({
 // NEW_MATER6IAL.alphaMap = DOOR_ALPHA_TEXTURE;
 // NEW_MATER6IAL.side = THREE.DoubleSide;
 
-const SPHERE_FORM = new THREE.Mesh(
+/* MESH */
+const CubeClone = Cube.clone();
+const SphereForm = new THREE.Mesh(
 	new THREE.SphereGeometry(0.5, 64, 64),
 	NEW_MATER6IAL
 );
-const PLANE_FORM = new THREE.Mesh(
+const PlaneForm = new THREE.Mesh(
 	new THREE.PlaneGeometry(1, 1, 100, 100),
 	NEW_MATER6IAL
 );
-
-const TORUS_FORM = new THREE.Mesh(
+const TorusForm = new THREE.Mesh(
 	new THREE.TorusGeometry(0.3, 0.2, 64, 128),
 	NEW_MATER6IAL
 );
-SPHERE_FORM.geometry.setAttribute(
+
+SphereForm.geometry.setAttribute(
 	"uv2",
-	new THREE.BufferAttribute(SPHERE_FORM.geometry.attributes.uv.array, 2)
+	new THREE.BufferAttribute(SphereForm.geometry.attributes.uv.array, 2)
 );
-PLANE_FORM.geometry.setAttribute(
+PlaneForm.geometry.setAttribute(
 	"uv2",
-	new THREE.BufferAttribute(PLANE_FORM.geometry.attributes.uv.array, 2)
+	new THREE.BufferAttribute(PlaneForm.geometry.attributes.uv.array, 2)
 );
-TORUS_FORM.geometry.setAttribute(
+TorusForm.geometry.setAttribute(
 	"uv2",
-	new THREE.BufferAttribute(TORUS_FORM.geometry.attributes.uv.array, 2)
+	new THREE.BufferAttribute(TorusForm.geometry.attributes.uv.array, 2)
 );
 
 const TriangleGeometry = new THREE.BufferGeometry();
@@ -184,33 +205,39 @@ TriangleGeometry.setAttribute(
 );
 const TriangleMesh = new THREE.Mesh(TriangleGeometry, TriangleMaterial);
 TriangleMesh.visible = false;
-const CubesGroup = new THREE.Group();
 // let savedTime = Date.now();
 
-// SCALES
+// GROUPE
+const CUBES_GROUP = new THREE.Group();
+const MESH_NEW_MATERIAL_GROUP = new THREE.Group();
+CUBES_GROUP.add(Cube, CubeClone);
+MESH_NEW_MATERIAL_GROUP;
+
+/* UPDATE MESH PROPERTIES */
+/* Material */
+CubeClone.material.color = new THREE.Color();
+CubeClone.material.map = DOOR_COLOR_TEXTURE;
+
+/* Scales */
 Cube.scale.set(0.5, 0.5, 0.5);
 
-// POSITIONS
+/* Positions */
 Cube.position.set(0, 0, 0);
 CubeClone.position.set(-3, -1, 0);
-SPHERE_FORM.position.set(-2, 3, 0);
-PLANE_FORM.position.set(0, 3, 0);
-TORUS_FORM.position.set(2, 3, 0);
+SphereForm.position.set(-2, 3, 0);
+PlaneForm.position.set(0, 3, 0);
+TorusForm.position.set(2, 3, 0);
 
-// ROTATIONS
+/* Rotations */
 Cube.rotation.reorder("YXZ");
 Cube.rotation.set(Math.PI / 5, 3, 3);
 CubeClone.rotation.set(Math.PI / 2, -1, 0, "YXZ");
-// CubesGroup.rotation.z = 3;
-console.log(Cube);
+// CUBES_GROUP.rotation.z = 3;
 
-// GROUPE
-CubesGroup.add(Cube, CubeClone);
-
-// CLOCK
+/* CLOCK */
 const ANIMATION_CLOCK = new THREE.Clock();
 
-// LIGHT
+/* LIGHT */
 const AMBIENT_LIGHT = new THREE.AmbientLight(0xffffff, 0.5);
 const POINT_LIGHT = new THREE.PointLight(0xffffff, 0.5);
 POINT_LIGHT.position.x = 2;
@@ -223,31 +250,110 @@ const APP = initThreeJs({
 	axesSizes: 5,
 });
 
-APP.scene.add(CubesGroup);
+/* Scene */
+APP.scene.add(CUBES_GROUP);
 APP.scene.add(TriangleMesh);
-APP.scene.add(SPHERE_FORM);
-APP.scene.add(PLANE_FORM);
-APP.scene.add(TORUS_FORM);
+APP.scene.add(SphereForm);
+APP.scene.add(PlaneForm);
+APP.scene.add(TorusForm);
 APP.scene.add(AMBIENT_LIGHT);
 APP.scene.add(POINT_LIGHT);
 
-//
+/* Camera */
 APP.camera.position.z = 5;
 
-// GSAP
-// GSAP.to(CubesGroup.position, { duration: 0.2, delay: 1, x: 1 });
-// GSAP.to(CubesGroup.position, { duration: 0.2, delay: 2, x: 0 });
+/* Control */
+APP.control.enableDamping = true;
 
-// CURSOR ANIMATION
+/* Animate */
+APP.animate(() => {
+	// Animation using native js date
+	// const CURRENT_TIME = Date.now();
+	// const DELTA_TIME = CURRENT_TIME - savedTime;
+	// savedTime = CURRENT_TIME;
+
+	// ANIMATION using THREE clock
+	const ELAPSED_TIME = ANIMATION_CLOCK.getElapsedTime();
+	CUBES_GROUP.rotation.y = Math.sin(ELAPSED_TIME);
+	CUBES_GROUP.rotation.x = Math.cos(ELAPSED_TIME);
+
+	// SphereForm.rotation.y = 0.1 * ELAPSED_TIME;
+	// PlaneForm.rotation.y = 0.1 * ELAPSED_TIME;
+	// TorusForm.rotation.y = 0.1 * ELAPSED_TIME;
+
+	// SphereForm.rotation.x = 0.25 * ELAPSED_TIME;
+	// PlaneForm.rotation.x = 0.25 * ELAPSED_TIME;
+	// TorusForm.rotation.x = 0.25 * ELAPSED_TIME;
+
+	// CAMERA ANIMATION
+	// APP.camera.position.x = Math.sin(CURSOR_POS.x * Math.PI * 2) * 5;
+	// APP.camera.position.z = Math.cos(CURSOR_POS.x * Math.PI * 2) * 5;
+	// APP.camera.position.y = CURSOR_POS.y * 10;
+	// APP.camera.lookAt(new THREE.Vector3());
+
+	// UPDATE CONTROL
+	APP.control.update();
+});
+
+/* ANIMATIONS */
+// GSAP
+// GSAP.to(CUBES_GROUP.position, { duration: 0.2, delay: 1, x: 1 });
+// GSAP.to(CUBES_GROUP.position, { duration: 0.2, delay: 2, x: 0 });
+
+/* Cursor Animation*/
 // const CURSOR_POS = {
 // 	x: 0,
 // 	y: 0,
 // };
-// window.addEventListener("mousemove", (e) => {
-// 	CURSOR_POS.x = e.clientX / APP.sceneSizes.width - 0.5;
-// 	CURSOR_POS.y = e.clientY / APP.sceneSizes.height - 0.5;
-// });
 
+/* DEBUGGER UI */
+const _GUI_CUBES_GROUP_FOLDER = _GUI.addFolder("Cube group");
+_GUI_CUBES_GROUP_FOLDER
+	.add(CUBES_GROUP.position, "y")
+	.min(-100)
+	.max(100)
+	.step(0.01);
+_GUI_CUBES_GROUP_FOLDER.add(Cube.material, "wireframe").name("Cube wireframe");
+_GUI_CUBES_GROUP_FOLDER.addColor(Cube.material, "color").name("Cube color");
+_GUI_CUBES_GROUP_FOLDER
+	.add(
+		{
+			function: () => {
+				GSAP.to(CubeClone.rotation, {
+					duration: 1,
+					y: CubeClone.rotation.y + Math.PI * 2,
+				});
+				GSAP.to(Cube.rotation, {
+					duration: 1,
+					y: Cube.rotation.y + Math.PI * 2,
+				});
+			},
+		},
+		"function"
+	)
+	.name("Cubes lite animation");
+
+const _GUI_TRIANGLE_MESH_FOLDER = _GUI.addFolder("Triangle");
+_GUI_TRIANGLE_MESH_FOLDER
+	.add(TriangleMesh, "visible")
+	.name("Triangle Mesh Visible");
+_GUI_TRIANGLE_MESH_FOLDER
+	.add(TriangleMaterial, "wireframe")
+	.name("Triangle Wireframe");
+
+const _GUI_NEW_MATERIAL_FOLDER = _GUI.addFolder("New Material props");
+_GUI_NEW_MATERIAL_FOLDER
+	.add(NEW_MATER6IAL, "metalness")
+	.min(0)
+	.max(1)
+	.step(0.0001);
+_GUI_NEW_MATERIAL_FOLDER
+	.add(NEW_MATER6IAL, "roughness")
+	.min(0)
+	.max(1)
+	.step(0.0001);
+
+/* JS EVENTS */
 window.addEventListener("dblclick", () => {
 	const fullscreenElement =
 		// @ts-ignore: Safari fix ಥ‿ಥ
@@ -272,28 +378,6 @@ window.addEventListener("dblclick", () => {
 	}
 });
 
-// CONTROLS
-_GUI.add(CubesGroup.position, "y").min(-100).max(100).step(0.01);
-_GUI.add(TriangleMesh, "visible");
-_GUI.add(TriangleMaterial, "wireframe");
-_GUI.add(Cube.material, "wireframe");
-_GUI.addColor(Cube.material, "color");
-_GUI.add(
-	{
-		function: () => {
-			GSAP.to(CubeClone.rotation, {
-				duration: 1,
-				y: CubeClone.rotation.y + Math.PI * 2,
-			});
-			GSAP.to(Cube.rotation, { duration: 1, y: Cube.rotation.y + Math.PI * 2 });
-		},
-	},
-	"function"
-);
-const _GUI_MATERIAL_FOLDER = _GUI.addFolder("New Material props");
-_GUI_MATERIAL_FOLDER.add(NEW_MATER6IAL, "metalness").min(0).max(1).step(0.0001);
-_GUI_MATERIAL_FOLDER.add(NEW_MATER6IAL, "roughness").min(0).max(1).step(0.0001);
-
 window.addEventListener("keydown", (e) => {
 	console.log("pressend h");
 	if (e.key === "h") {
@@ -302,32 +386,7 @@ window.addEventListener("keydown", (e) => {
 	}
 });
 
-APP.control.enableDamping = true;
-APP.animate(() => {
-	// Animation using native js date
-	// const CURRENT_TIME = Date.now();
-	// const DELTA_TIME = CURRENT_TIME - savedTime;
-	// savedTime = CURRENT_TIME;
-
-	// ANIMATION using THREE clock
-	const ELAPSED_TIME = ANIMATION_CLOCK.getElapsedTime();
-	CubesGroup.rotation.y = Math.sin(ELAPSED_TIME);
-	CubesGroup.rotation.x = Math.cos(ELAPSED_TIME);
-
-	// SPHERE_FORM.rotation.y = 0.1 * ELAPSED_TIME;
-	// PLANE_FORM.rotation.y = 0.1 * ELAPSED_TIME;
-	// TORUS_FORM.rotation.y = 0.1 * ELAPSED_TIME;
-
-	// SPHERE_FORM.rotation.x = 0.25 * ELAPSED_TIME;
-	// PLANE_FORM.rotation.x = 0.25 * ELAPSED_TIME;
-	// TORUS_FORM.rotation.x = 0.25 * ELAPSED_TIME;
-
-	// CAMERA ANIMATION
-	// APP.camera.position.x = Math.sin(CURSOR_POS.x * Math.PI * 2) * 5;
-	// APP.camera.position.z = Math.cos(CURSOR_POS.x * Math.PI * 2) * 5;
-	// APP.camera.position.y = CURSOR_POS.y * 10;
-	// APP.camera.lookAt(new THREE.Vector3());
-
-	// UPDATE CONTROL
-	APP.control.update();
-});
+// window.addEventListener("mousemove", (e) => {
+// 	CURSOR_POS.x = e.clientX / APP.sceneSizes.width - 0.5;
+// 	CURSOR_POS.y = e.clientY / APP.sceneSizes.height - 0.5;
+// });
