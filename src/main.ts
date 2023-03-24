db334103d2d40aadba78b6a5bc2f3ac81cc7d9d5
Neosoulink <neodarksoulink@gmail.com>
2023-03-24 18:13:21 +0300

    refactor: minor

--- src/main.ts
+++ src/main.ts
@@ -175,6 +175,7 @@ const ENVIRONMENT_MAP_TEXTURE = CUBE_TEXTURE_LOADER.load([
 	pzEnvImg,
 	nzEnvImg,
 ]);
+ENVIRONMENT_MAP_TEXTURE.encoding = THREE.sRGBEncoding;
 // const BAKED_SHADOW = TEXTURE_LOADER.load(bakedShadowImg);
 const SIMPLE_SHADOW = TEXTURE_LOADER.load(simpleShadowImg);
 /* Update texture properties */
